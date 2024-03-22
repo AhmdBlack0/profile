ae4d9641c7a77a55444fd3f1caf356a47528374b
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2024-03-22 07:47:00 +0200

    Add files via upload

---
+++ profile/skills.js
@@ -0,0 +1,20 @@
+let btn = document.querySelectorAll("#btn");
+let sum = document.querySelectorAll("#summary");
+
+function fun (bt, su) {
+    bt.onclick = function () {
+        if (su.style.display === "flex") {
+            bt.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
+            su.style.display = "none";
+            
+        } else {
+            su.style.display = "flex"
+            su.style.justifyContent = "space-between";
+            su.style.alignItems = "center";
+            bt.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
+        }
+    }
+}
+for (let i = 0; i < btn.length; i++) {
+    fun(btn[i], sum[i]);
+}
